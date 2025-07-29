fa87d9c85cc6abf0d809da711ab8687bf65aff49
mnoornawab <mohdnoor.nawab@gmail.com>
2025-07-29 18:52:28 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VisitForm from './components/VisitForm';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Customer Visit Management</h1>
+      <VisitForm />
+    </div>
+  );
+}
+
+export default App;
